f7802a4501a3e71201a24b352f0efe5c415afc9c
Vladimir Balko <vbalko@gmail.com>
2018-09-20 23:53:45 +0200

    Functional call to the backend api

--- api/router.js
+++ api/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = new express.Router();
 
 //ROOT
-router.get("/",(req,res) => res.json({"root":"test1"}));
+router.get("/api",(req,res) => res.json({"root":"test1"}));
 
 var handleRoot = (req,res) => {
     return res.json({"root":"test"});
--- ui5/controller/app.controller.js
+++ ui5/controller/app.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
     "sap/ui/demo/basicTemplate/model/formatter"
-], function (Controller, formatter) {
+], function (Controller, JSONModel, formatter) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
@@ -9,7 +10,39 @@ sap.ui.define([
         formatter: formatter,
 
         onInit: function () {
+            this._load()
+        },
 
+        _mapResults(results) {
+            const oModel = new JSONModel();
+            oModel.setData(results);
+            this.getView().setModel(oModel);
+        },
+        
+        _load() {
+            const oView = this.getView();
+            const sUrl = './api';
+
+            const self = this;
+
+            $.get(sUrl, {})
+            .done(function(results) {
+                console.log("result: ",results);
+                self._mapResults(results);
+            })
+            .fail(function(err) {
+                if (err !== undefined) {
+                    var oErrorResponse = $.parseJSON(err.responseText);
+
+                    sap.m.MessageToast.show(oErrorResponse.message, {
+                        duration: 6000
+                    });
+                } else {
+                    sap.m.MessageToast.show("Unknown error!");
+                }
+                    
+                //} 
+            });
         }
     });
 });
