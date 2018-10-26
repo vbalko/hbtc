50ecc723d98ff1a2d13f885575b36cc36a08c957
Vladimir Balko <vbalko@gmail.com>
2018-10-26 23:27:01 +0200

    UI5 - table show all results (previously top 100)

--- ui5/controller/app.controller.js
+++ ui5/controller/app.controller.js
@@ -1,48 +1,49 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/demo/basicTemplate/model/formatter"
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/json/JSONModel',
+	'sap/ui/demo/basicTemplate/model/formatter'
 ], function (Controller, JSONModel, formatter) {
-    "use strict";
+	'use strict';
 
-    return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
+	return Controller.extend('sap.ui.demo.basicTemplate.controller.App', {
 
-        formatter: formatter,
+		formatter: formatter,
 
-        onInit: function () {
-            this._load()
-        },
+		onInit: function () {
+			this._load();
+		},
 
-        _mapResults(results) {
-            const oModel = new JSONModel();
-            oModel.setData(results);
-            this.getView().setModel(oModel);
-        },
+		_mapResults(results) {
+			const oModel = new JSONModel();
+			oModel.setSizeLimit(results.length);
+			oModel.setData(results);
+			this.getView().setModel(oModel);
+		},
         
-        _load() {
-            const oView = this.getView();
-            const sUrl = './api';
+		_load() {
+			const oView = this.getView();
+			const sUrl = './api';
 
-            const self = this;
+			const self = this;
 
-            $.get(sUrl, {})
-            .done(function(results) {
-                console.log("result: ",results);
-                self._mapResults(results);
-            })
-            .fail(function(err) {
-                if (err !== undefined) {
-                    var oErrorResponse = $.parseJSON(err.responseText);
+			$.get(sUrl, {})
+				.done(function(results) {
+					console.log('result: ',results);
+					self._mapResults(results);
+				})
+				.fail(function(err) {
+					if (err !== undefined) {
+						var oErrorResponse = $.parseJSON(err.responseText);
 
-                    sap.m.MessageToast.show(oErrorResponse.message, {
-                        duration: 6000
-                    });
-                } else {
-                    sap.m.MessageToast.show("Unknown error!");
-                }
+						sap.m.MessageToast.show(oErrorResponse.message, {
+							duration: 6000
+						});
+					} else {
+						sap.m.MessageToast.show('Unknown error!');
+					}
                     
-                //} 
-            });
-        }
-    });
+					//} 
+				});
+		}
+	});
 });
